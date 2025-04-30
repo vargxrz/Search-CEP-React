e452b5d44f012ce3d360b772c11ef4ec77763192
vargas <vargasvargasjoa@gmail.com>
2025-04-30 14:14:25 -0300

    adjustment

--- src/App.js
+++ src/App.js
@@ -1,57 +1,61 @@
-import { FiSearch } from "react-icons/fi"
-import "./styles.css"
-import "./index.css"
-import { useState } from "react";
+import {FiSearch} from "react-icons/fi";
+import "./styles.css";
+import "./index.css";
+import {useState} from "react";
 
-import api from "./services/api"
+import api from "./services/api";
 
 function App() {
+    const [input, setInput] = useState("");
+    const [cep, setCep] = useState({});
 
-  const [input, setInput] = useState("")
-  const [cep, setCep] = useState({})
+    async function handleSearch() {
+        const formattedCep = input.replace("-", "");
 
- async function handleSearch() {
-  if (input === "") {
-    alert("Preencha algum cep!")
-    return;
-  }
+        if (formattedCep === "") {
+            alert("Preencha algum CEP!");
+            return;
+        }
 
-  try {
-    const response = await api.get(`${input}/json`);
-    console.log(response)
-    setCep(response.data)
-    setInput("");
-  }
-  catch {
-    alert("Ops, erro ao buscar!")
-    setInput("")
-  }
-}
+        try {
+            const response = await api.get(`${formattedCep}/json`);
+            console.log(response);
+            setCep(response.data);
+            setInput("");
+        } catch {
+            alert("Ops, erro ao buscar!");
+            setInput("");
+        }
+    }
 
-  return (
-    <div className="container">
-      <h1 className="title">Buscador CEP</h1>
-      <div className="containerInput">
-        
-        <input type="text" placeholder="Digite seu cep..." value={input} 
-        onChange={(e) => setInput(e.target.value)}/>
+    return (
+        <div className="container">
+            <h1 className="title">Buscador CEP</h1>
+            <div className="containerInput">
+                <input
+                    type="text"
+                    placeholder="Digite seu CEP..."
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                />
 
-        <button className="buttonSearch" onClick={handleSearch}>
-          <FiSearch size={25} color="#FFF"/>
-        </button>
+                <button className="buttonSearch" onClick={handleSearch}>
+                    <FiSearch size={25} color="#FFF"/>
+                </button>
+            </div>
 
-      </div>
-
-      {Object.keys(cep).length > 0 && (
-      <main className="main">
-        <h2>{cep.cep}</h2>
-        <span>{cep.logradouro}</span>
-        <span>Bairro: {cep.bairro}</span>
-        <span>{cep.localidade} - {cep.uf}</span>
-      </main>
-      )}
-    </div>
-  );
+            {Object.keys(cep).length > 0 && (
+                <main className="main">
+                    <h2>{cep.cep}</h2>
+                    <span>{cep.logradouro}</span>
+                    <span>Bairro: {cep.bairro}</span>
+                    <span>
+            {cep.localidade} - {cep.uf}
+          </span>
+                </main>
+            )}
+        </div>
+    );
 }
 
 export default App;
